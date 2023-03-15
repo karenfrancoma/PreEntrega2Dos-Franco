355fb3efd7f436d909e17b8ba3229c8b8d4293d7
karenfranco <karendfrancom@gmail.com>
2023-03-14 19:15:20 -0500

    todo listo

--- js/script.js
+++ js/script.js
@@ -1,14 +1,13 @@
-//Objeto
 
+//FUNCIÓN CONSTRUCTORA
 class Ticket {
-  constructor(locacion, precio) {
+  constructor(locacion, valor) {
     this.locacion = locacion;
-    this.precio = parseInt(precio);
+    this.valor = parseInt(valor);
   }
 }
 
-//Arrays
-
+// Arrays
 const tickets = [
   new Ticket("Norte", 100),
   new Ticket("Oriental", 200),
@@ -16,52 +15,79 @@ const tickets = [
   new Ticket("VIP", 900),
 ];
 
-console.log(tickets);
+alert('Bienvenido/a. Aquí puedes adquirir tus tickets para Music of the Spheres World Tour');
+
+// FUNCTIONS
+let multiplicacion = (a, b) => a * b;
+
+//HIGHER-ORDER FUNCTION
+const ticketList = tickets.reduce((acc, ticket) => `${acc}\n${ticket.locacion}: $${ticket.valor}`, '');
+alert(`Tickets disponibles:${ticketList}`);
 
 
-let comprar = true;
+//BUCLE DO-WHILE
 
-while (comprar) {
+let opcion;
+do {
+  opcion = parseInt(prompt("¿Quieres comprar tickets? \n1 - Sí \n2 - No"));
 
-    let cantidadTickets = parseInt(prompt('Ingresa la cantidad de tickets que deseas: \n1 ticket \n2 tickets \nSi no quieres comprar, ingresa x para finalizar'));
+  if (opcion === 1) {
 
-    if (cantidadTickets.toUpperCase() == 'X') {
-        comprar = false;
-        break;
-    }
+    let locacion = parseInt(
+      prompt(
+        "Elige tu locación: \n1 - Norte \n2 - Oriental \n3 - Occidental \n4 - VIP"
+      )
+    );
+    let cantidadTickets = parseInt(
+      prompt(
+        "Ingresa la cantidad de tickets que deseas: \n1 ticket \n2 tickets"
+      )
+    );
 
-    let cantidadMax = 2;
-    let cantidadMin = 1;
+    function comprar(locacion, cantidadTickets) {
+      let precio;
 
-    switch (cantidadTickets) {
-        case '1':
-            let multiplicacion = (a,b) => a * b;
-            let total = multiplicacion(cantidadMax, tickets[0]);           
-            break;
-    
+      switch (locacion) {
+        case 1:
+          precio = tickets[0].valor;
+          break;
+        case 2:
+          precio = tickets[1].valor;
+          break;
+        case 3:
+          precio = tickets[2].valor;
+          break;
+        case 4:
+          precio = tickets[3].valor;
+          break;
         default:
-            break;
+          return 0;
+      }
+
+      let total = multiplicacion(precio, cantidadTickets);
+      return total;
     }
 
+    let totalCompra = comprar(locacion, cantidadTickets);
+    alert("El total de tu compra es: $" + totalCompra);
+
+    alert("Gracias por tu compra");
+    break;
+  }
+
+
+} while (opcion !== 2);
+
+if (opcion === 2) {
+  alert('Fin del proceso')
 }
 
-/* //Conditional
+// Array with ticket values
+const ticketValues = tickets.map((ticket) => ticket.valor);
 
-let cantidadTickets = parseInt(
-  prompt("Ingresa la cantidad de tickets que deseas. Cantidad máxima: 2")
-);
+// Calculate total value of tickets using reduce()
+const totalTicketValue = ticketValues.reduce((acc, curr) => acc + curr, 0);
 
-let cantidadMax = 2;
-let cantidadMin = 1;
-
-if (cantidadTickets == cantidadMax) {
-  alert("Elige tu locación");
-} else if (cantidadTickets == cantidadMin) {
-  alert("Elige tu locación");
-} else {
-  alert("Opción invalida");
-}
-
-//Functio */ns
-
+// Display total value of tickets
+console.log(`The total value of all tickets is: $${totalTicketValue}`);
 
